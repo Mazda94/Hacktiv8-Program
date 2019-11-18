80e9e7e900a6100d494e1ca60cf688690010b4cb
Mazda94 <m.alli9401@gmail.com>
2019-11-18 23:16:09 +0700

    Update repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hacktiv8-Program
+Repository ini merupakan dokumentasi program hacktiv8 yang saya ikuti
---
+++ phase-0/week-1/belajar web design/quotes.js
@@ -0,0 +1,5 @@
+if (stuck == true) {
+    console.log('Ambil air wudhu dan bersujudlah');
+} else {
+    console.log('Stay humble and keep istiqomah... :)');
+}
