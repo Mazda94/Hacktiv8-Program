dbde6da30f8f03779db77154da584f678249f604
Mazda94 <m.alli9401@gmail.com>
2019-11-24 13:56:37 +0700

    Add ArrayLoop.js

--- phase-0/week-3/ArrayLoop.js
+++ phase-0/week-3/ArrayLoop.js
@@ -9,6 +9,31 @@
 // Buatlah sebuah fungsi dengan nama dataHandlingdengan sebuah parameter untuk menerima argumen. 
 // Argumen yang akan diterima adalah sebuah array yang berisi beberapa array sejumlah n.
 
+function dataHandling(arr) {
+    var resultArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        var newArr = [];
+        for (let j = 0; j < arr[i].length; j++) {
+            switch (j) {
+                case 0:
+                    newArr.push('Nomor Id : ' + arr[i][j])
+                    break;
+                case 1:
+                    newArr.push('Nama lengkap : ' + arr[i][j])
+                    break
+                case 2:
+                    newArr.push('TTL : ' + arr[i][j] + ' ' + arr[i][j + 1])
+                    break
+                case 4:
+                    newArr.push('Hobi : ' + arr[i][j])
+                    break
+            }
+        }
+        resultArray.push(newArr)
+    }
+    return resultArray
+}
+
 var input = [
     ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
     ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
@@ -16,27 +41,12 @@ var input = [
     ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
 ]
 
-console.log(dataHandling(input))
-
-function dataHandling(arr) {
-    var newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            switch (j) {
-                case 0:
-                    newArr.push('Nomor Id :', arr[i][j])
-                    break;
-                case 1:
-                    newArr.push('Nama lengkap :', arr[i][j])
-                    break
-                case 2:
-                    console.log('TTL :', arr[i][j])
-                    break
-                case 3:
-                    console.log('Hobi :', arr[i][j])
-                    break
-            }
-        }
+var newArray = dataHandling(input)
+for (let i = 0; i < newArray.length; i++) {
+    console.log('*********** Data ke-' + i + ' ***********')
+    for (let j = 0; j < newArray[i].length; j++) {
+        console.log(newArray[i][j])
     }
-    return String(arr)
+    console.log('*******************************')
+    console.log('')
 }
